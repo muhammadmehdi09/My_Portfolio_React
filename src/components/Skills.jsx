a305a438a7a92fd5885a1cde90bd9ff309f105ec
Coding Verse <120133578+muhammadmehdi09@users.noreply.github.com>
2025-12-19 20:14:23 +0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -40,7 +40,40 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="mx-auto">
+      <div className="mx-auto">
+          <div className='text-[#f0f0e6] mb-2'>
+            <span className="text-base font-medium">JAVASCRIPT</span>
+          </div>
+          <div
+            className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
+            <span
+              className='w-[85%] after:content-["85%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 javascript'></span>
+          </div>
+        </div>
+
+      <div className="mx-auto">
+          <div className='text-[#f0f0e6] mb-2'>
+            <span className="text-base font-medium">PYTHON</span>
+          </div>
+          <div
+            className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
+            <span
+              className='w-[85%] after:content-["85%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 bootstrap'></span>
+          </div>
+        </div>
+
+      <div className="mx-auto">
+          <div className='text-[#f0f0e6] mb-2'>
+            <span className="text-base font-medium">Chatbot Developement</span>
+          </div>
+          <div
+            className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
+            <span
+              className='w-[85%] after:content-["85%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 bootstrap'></span>
+          </div>
+        </div>
+
+      <div className="mx-auto">
           <div className='text-[#f0f0e6] mb-2'>
             <span className="text-base font-medium">Full MERN Stack</span>
           </div>
@@ -51,14 +84,14 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="mx-auto">
+      <div className="mx-auto">
           <div className='text-[#f0f0e6] mb-2'>
-            <span className="text-base font-medium">JAVASCRIPT</span>
+            <span className="text-base font-medium">Machine Learning</span>
           </div>
           <div
             className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
             <span
-              className='w-[75%] after:content-["75%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 javascript'></span>
+              className='w-[70%] after:content-["70%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 bootstrap'></span>
           </div>
         </div>
 
@@ -70,17 +103,6 @@ const Skills = () => {
             className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
             <span
               className='w-[60%] after:content-["60%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 bootstrap'></span>
-          </div>
-        </div>
-
-        <div className="mx-auto">
-          <div className='text-[#f0f0e6] mb-2'>
-            <span className="text-base font-medium">PYTHON</span>
-          </div>
-          <div
-            className='relative h-2.5 w-[200px] min-[300px]:w-[250px] min-[400px]:w-[350px] sm:w-[500px] md:w-[600px] lg:w-[700px] bg-[#f0f0f0] origin-left animate-[animate_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0'>
-            <span
-              className='w-[50%] after:content-["50%"] absolute bg-[#25b79f] h-full origin-left animate-[animate_1s_1s_cubic-bezier(1,0,0.5,1)_forwards] rounded-[10px] scale-x-0  before:absolute before:content-[""] before:h-0 before:w-0 before:border-r-0 before:border-t-[#f0f0f0] before:border-b-0 before:border-[7px] before:border-solid before:border-transparent before:right-0 before:-top-3 after:absolute after:text-[#07374a] after:text-xs after:font-bold after:bg-[#f0f0f0] after:px-2 after:py-px after:rounded-[3px] after:right-0 after:-top-7 bootstrap'></span>
           </div>
         </div>
         
@@ -100,3 +122,4 @@ const Skills = () => {
 }
 
 export default Skills
+
