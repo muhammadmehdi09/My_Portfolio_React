265f959ba3089e394e30f44aaac1556aafae049a
Coding Verse <120133578+muhammadmehdi09@users.noreply.github.com>
2025-12-23 12:42:50 +0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,11 +9,11 @@ const Projects = () => {
       <div className="grid xl:grid-cols-3 grid-cols-1 w-fit mx-auto mb-44">
         <div className="bg-[#07374a] w-fit p-4 m-5 rounded-md hover:scale-110 duration-500">
           <img src="Images/Project1.jpg" className="w-[450px]" alt='' />
-          <p className="font-semibold text-base sm:text-2xl text-center text-[#25b79f] mt-2">Ecomerce Store(Frotend)</p>
+          <p className="font-semibold text-base sm:text-2xl text-center text-[#25b79f] mt-2">TextUtils</p>
           <div className="mx-auto w-fit mt-2 grid grid-cols-1 xl:grid-cols-2">
-            <a href="https://mrubiks-main.vercel.app"><button
+            <a href="https://textutilsx.vercel.app/"><button
               className="bg-green-500 rounded-xl px-9 py-3 text-[#f0f0e6] font-semibold text-xl m-1">Live Demo</button></a>
-            <a href="https://github.com/muhammadmehdi09/MRUBIKS">
+            <a href="https://github.com/muhammadmehdi09/textutils/">
               <button className="bg-red-500 rounded-xl px-7 text-[#f0f0e6] py-3 font-semibold text-xl m-1">Source
                 Code</button>
             </a>
@@ -47,5 +47,6 @@ const Projects = () => {
     </section>
   )
 }
+
 
 export default Projects
