34f677968bb3a4988d16871ebe5bb365ccad1f67
Coding Verse <120133578+muhammadmehdi09@users.noreply.github.com>
2025-12-23 12:51:44 +0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
       </div>
       <div className="grid xl:grid-cols-3 grid-cols-1 w-fit mx-auto mb-44">
         <div className="bg-[#07374a] w-fit p-4 m-5 rounded-md hover:scale-110 duration-500">
-          <img src="Images/Project1.jpg" className="w-[450px]" alt='' />
+          <img src="Images/Project1.png" className="w-[450px]" alt='' />
           <p className="font-semibold text-base sm:text-2xl text-center text-[#25b79f] mt-2">TextUtils</p>
           <div className="mx-auto w-fit mt-2 grid grid-cols-1 xl:grid-cols-2">
             <a href="https://textutilsx.vercel.app/"><button
@@ -50,5 +50,6 @@ const Projects = () => {
 
 
 export default Projects
+
 
 
